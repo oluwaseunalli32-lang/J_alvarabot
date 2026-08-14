58f600b930f26c1434a89d9d0da6dd9cb29fff4c
oluwaseunalli32-lang <oluwaseunalli32@gmail.com>
2026-08-14 18:14:46 +0100

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -8,5 +8,3 @@ from .quiz import quiz, handle_callback as quiz_callback
 from .grammar import grammar, handle_callback as grammar_callback
 from .example import start as example_start, get_example, cancel as example_cancel, WAITING_FOR_WORD as EXAMPLE_WAITING
 from .define import start as define_start, get_definition, cancel as define_cancel, WAITING_FOR_WORD as DEFINE_WAITING
-
-# We'll also export the conversation states for use in bot.py
